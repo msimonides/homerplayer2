c60e9a75e5da565ae436c1b3bad9326b77a7ecce
Marcin Simonides <marcin@studio4plus.com>
2024-05-21 11:15:44 +0200

    Bump up version.

--- kiosksetup/build.gradle.kts
+++ kiosksetup/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.studio4plus.homerplayer2.kiosk"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
