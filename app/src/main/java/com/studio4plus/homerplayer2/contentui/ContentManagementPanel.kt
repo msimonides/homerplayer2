8ff0d5af426730111cfb056e403238803e7a58ce
Marcin Simonides <marcin@studio4plus.com>
2024-10-19 13:03:08 +0200

    Add confirmation dialog when removing content.

--- app/src/main/java/com/studio4plus/homerplayer2/contentui/ContentManagementPanel.kt
+++ app/src/main/java/com/studio4plus/homerplayer2/contentui/ContentManagementPanel.kt
@@ -27,12 +27,18 @@ package com.studio4plus.homerplayer2.contentui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,6 +89,7 @@ fun ContentManagementPanel(
             }
         }
 
+        var removeDialogAction by remember { mutableStateOf<(() -> Unit)?>(null) }
         LazyColumn {
             if (state.folders.isNotEmpty()) {
                 item { SectionTitle(R.string.content_section_title_audiobooks ) }
@@ -90,7 +97,10 @@ fun ContentManagementPanel(
                     state.folders,
                     key = AudiobookFolderViewState::uri,
                     itemContent = { item ->
-                        AudiobookFolderRow(folder = item, onRemoveClicked = { onRemoveFolder(item) })
+                        AudiobookFolderRow(
+                            folder = item,
+                            onRemoveClicked = { removeDialogAction = { onRemoveFolder(item) } }
+                        )
                     }
                 )
             }
@@ -105,12 +115,42 @@ fun ContentManagementPanel(
                             item,
                             dateFormatter,
                             onEditClicked = onEditPodcast,
-                            onRemoveClicked = { onRemovePodcast(item) },
+                            onRemoveClicked = { removeDialogAction = { onRemovePodcast(item) } },
                         )
                     }
                 )
             }
         }
+        if (removeDialogAction != null) {
+            ConfirmRemoveDialog(
+                onRemove = { removeDialogAction?.invoke() },
+                onDismiss = { removeDialogAction = null })
+        }
+    }
+}
+
+@Composable
+private fun ConfirmRemoveDialog(
+    onRemove: () -> Unit,
+    onDismiss: () -> Unit,
+) {
+    DefaultAlertDialog(
+        onDismissRequest = onDismiss,
+        buttons = {
+            TextButton(onDismiss) { Text(stringResource(R.string.generic_dialog_cancel)) }
+            val removeAndDismiss = {
+                onRemove()
+                onDismiss()
+            }
+            TextButton(removeAndDismiss) {
+                Text(stringResource(R.string.content_dialog_remove_button_remove))
+            }
+        }
+    ) { horizontalPadding ->
+        Text(
+            stringResource(R.string.content_dialog_remove_message),
+            modifier = Modifier.padding(horizontal = horizontalPadding)
+        )
     }
 }
 
