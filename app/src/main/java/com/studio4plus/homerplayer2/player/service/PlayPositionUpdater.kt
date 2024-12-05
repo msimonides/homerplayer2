c82b670ffb63e3b381b81067170bc75887fd95ba
Marcin Simonides <marcin@studio4plus.com>
2024-12-05 15:00:50 +0100

    Update current position every few seconds.

--- app/src/main/java/com/studio4plus/homerplayer2/player/service/PlayPositionUpdater.kt
+++ app/src/main/java/com/studio4plus/homerplayer2/player/service/PlayPositionUpdater.kt
@@ -24,13 +24,24 @@
 
 package com.studio4plus.homerplayer2.player.service
 
+import android.net.Uri
 import androidx.media3.common.Player
 import com.studio4plus.homerplayer2.audiobooks.AudiobooksDao
 import com.studio4plus.homerplayer2.player.PlaybackUiStateRepository
+import com.studio4plus.homerplayer2.utils.tickerFlow
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.core.annotation.Factory
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Factory
 class PlayPositionUpdater(
     private val mainScope: CoroutineScope,
@@ -40,26 +51,62 @@ class PlayPositionUpdater(
 
     // TODO: handle changes via notification while paused.
 
+    private data class Position(val fileUri: Uri, val position: Long)
+
+    private sealed interface State {
+        data class Playing(val player: Player) : State
+        data class Stopped(val lastPosition: Position?) : State
+    }
+
+    private val stateFlow = MutableStateFlow<State>(State.Stopped(null))
+
+    init {
+        stateFlow.flatMapLatest { state ->
+            when (state) {
+                is State.Playing -> {
+                    // Update selected book to always match the most recently played one.
+                    updateLastSelectedBook(state.player)
+                    tickerFlow(5_000L)
+                        .mapNotNull {
+                            val currentFileUri = state.player.currentMediaItem?.localConfiguration?.uri
+                            currentFileUri?.let {
+                                Position(it, state.player.currentPosition)
+                            }
+                        }
+                }
+                is State.Stopped ->
+                    if (state.lastPosition != null) {
+                        flowOf(state.lastPosition)
+                    } else {
+                        emptyFlow()
+                    }
+            }
+        }.onEach { position ->
+            mainScope.launch {
+                audiobooksDao.updatePlayPosition(position.fileUri, position.position)
+            }
+        }.launchIn(mainScope)
+    }
+
     override fun onEvents(player: Player, events: Player.Events) {
         // onEvents is a more cumbersome API but it gets the Player.
         if (events.contains(Player.EVENT_IS_PLAYING_CHANGED)) {
-            onIsPlayingChanged(player, player.isPlaying)
+            if (player.isPlaying) {
+                stateFlow.value = State.Playing(player)
+            } else {
+                val currentFileUri = player.currentMediaItem?.localConfiguration?.uri
+                val position = currentFileUri?.let { Position(it, player.currentPosition) }
+                stateFlow.value = State.Stopped(lastPosition = position)
+            }
         }
     }
 
-    private fun onIsPlayingChanged(player: Player, isPlaying: Boolean) {
+    private fun updateLastSelectedBook(player: Player) {
         val currentFileUri = player.currentMediaItem?.localConfiguration?.uri ?: return
-        if (!isPlaying) {
-            mainScope.launch {
-                audiobooksDao.updatePlayPosition(currentFileUri, player.currentPosition)
-            }
-        } else {
-            mainScope.launch {
-                val currentFile = audiobooksDao.getAudiobookFile(currentFileUri)
-                if (currentFile != null) {
-                    // Update selected book to alywas match the most recently played one.
-                    playbackUiStateRepository.updateLastSelectedBookId(currentFile.bookId)
-                }
+        mainScope.launch {
+            val currentFile = audiobooksDao.getAudiobookFile(currentFileUri)
+            if (currentFile != null) {
+                playbackUiStateRepository.updateLastSelectedBookId(currentFile.bookId)
             }
         }
     }
