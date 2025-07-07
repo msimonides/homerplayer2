12a6e4e71c1db4577b7b07259c674caac6b49adc
Marcin Simonides <marcin@studio4plus.com>
2025-07-07 21:19:46 +0200

    Handle more podcast date formats.
    Also don't crash when parsing fails...

--- app/src/main/java/com/studio4plus/homerplayer2/podcasts/usecases/DownloadPodcastFeed.kt
+++ app/src/main/java/com/studio4plus/homerplayer2/podcasts/usecases/DownloadPodcastFeed.kt
@@ -24,6 +24,7 @@
 
 package com.studio4plus.homerplayer2.podcasts.usecases
 
+import androidx.annotation.VisibleForTesting
 import com.prof18.rssparser.RssParser
 import com.prof18.rssparser.exception.RssParsingException
 import com.prof18.rssparser.model.RssChannel
@@ -31,6 +32,7 @@ import com.prof18.rssparser.model.RssItem
 import com.studio4plus.homerplayer2.base.DispatcherProvider
 import com.studio4plus.homerplayer2.net.executeAwait
 import com.studio4plus.homerplayer2.podcasts.MAX_PODCAST_EPISODE_COUNT
+import io.sentry.Sentry
 import kotlinx.coroutines.runInterruptible
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
@@ -39,9 +41,16 @@ import org.koin.core.annotation.Factory
 import timber.log.Timber
 import java.io.IOException
 import java.net.UnknownHostException
+import java.time.DateTimeException
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.format.DateTimeParseException
+import java.time.format.SignStyle
+import java.time.temporal.ChronoField
 import javax.net.ssl.SSLException
 
 data class PodcastFeedEpisode(
@@ -126,8 +135,86 @@ class DownloadPodcastFeed(
             Result.ParseError
         }
 
-    private fun String?.parseRssDate(): Instant? =
-        this?.let {
-            ZonedDateTime.parse(this, DateTimeFormatter.RFC_1123_DATE_TIME).toInstant()
+    companion object {
+        private val dow = HashMap<Long, String>().apply {
+            put(1L, "Mon")
+            put(2L, "Tue")
+            put(3L, "Wed")
+            put(4L, "Thu")
+            put(5L, "Fri")
+            put(6L, "Sat")
+            put(7L, "Sun")
         }
+        private val moy = HashMap<Long, String>().apply {
+            put(1L, "Jan")
+            put(2L, "Feb")
+            put(3L, "Mar")
+            put(4L, "Apr")
+            put(5L, "May")
+            put(6L, "Jun")
+            put(7L, "Jul")
+            put(8L, "Aug")
+            put(9L, "Sep")
+            put(10L, "Oct")
+            put(11L, "Nov")
+            put(12L, "Dec")
+        }
+        private val obsoleteZones = HashMap<String, String>().apply {
+            put("UT", "GMT")
+            put("EDT", "-0400")
+            put("EST", "-0500")
+            put("CST", "-0600")
+            put("CDT", "-0500")
+            put("MST", "-0700")
+            put("MDT", "-0600")
+            put("PST", "-0800")
+            put("PDT", "-0700")
+        }
+
+        // Based on DateTimeFormatter.RFC_1123_DATE_TIME.
+        private val RFC_1123_WITH_NO_ZONE = DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .parseLenient()
+            .optionalStart()
+            .appendText(ChronoField.DAY_OF_WEEK, dow)
+            .appendLiteral(", ")
+            .optionalEnd()
+            .appendValue(ChronoField.DAY_OF_MONTH, 1, 2, SignStyle.NOT_NEGATIVE)
+            .appendLiteral(' ')
+            .appendText(ChronoField.MONTH_OF_YEAR, moy)
+            .appendLiteral(' ')
+            .appendValue(ChronoField.YEAR, 4)  // 2 digit year not handled
+            .appendLiteral(' ')
+            .appendValue(ChronoField.HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendLiteral(':')
+            .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+            .optionalEnd()
+            .appendLiteral(' ')
+            .toFormatter()
+
+        @VisibleForTesting
+        fun String?.parseRssDate(): Instant? =
+            this?.let { dateString ->
+                try {
+                    val parsed = try {
+                        ZonedDateTime.parse(dateString, DateTimeFormatter.RFC_1123_DATE_TIME)
+                    } catch(e :DateTimeParseException) {
+                        // Parse obsolete time zones.
+                        val zoneString = dateString.substringAfterLast(" ")
+                        val localDateString = dateString.dropLast(zoneString.length)
+                        val localDate = LocalDateTime.parse(localDateString, RFC_1123_WITH_NO_ZONE)
+                        val zone = ZoneId.of(zoneString, obsoleteZones)
+                        ZonedDateTime.of(localDate, zone)
+                    }
+                    parsed.toInstant()
+                } catch (e: DateTimeException) {
+                    Timber.w(e, "Failed to parse RSS date: '$dateString'")
+                    Sentry.captureException(e)
+                    null
+                }
+            }
+    }
 }
---
+++ app/src/test/java/com/studio4plus/homerplayer2/podcasts/PodcastDateTests.kt
@@ -0,0 +1,80 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Marcin Simonides
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.studio4plus.homerplayer2.podcasts
+
+import com.studio4plus.homerplayer2.podcasts.usecases.DownloadPodcastFeed.Companion.parseRssDate
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+class PodcastDateTests {
+
+    @Test
+    fun `parsing test`() {
+        val regularDates = listOf(
+            "Tue, 3 Jun 2008 11:05:30 GMT",
+            "Tue, 3 Jun 2008 11:05:30 +0100",
+            "3 Jun 2008 11:05:30 GMT",
+            "Tue, 3 Jun 2008 1:05:30 GMT",
+            "Tue, 3 Jun 2008 1:05 GMT",
+        )
+        val nonStandardDatesWithExpected = listOf(
+            "Tue, 3 Jun 2008 11:05:30 UT" to "Tue, 3 Jun 2008 11:05:30 GMT",
+            "Tue, 3 Jun 2008 11:05:30 EDT" to "Tue, 3 Jun 2008 11:05:30 -0400",
+            "Tue, 3 Jun 2008 11:05:30 EST" to "Tue, 3 Jun 2008 11:05:30 -0500",
+            "Tue, 3 Jun 2008 11:05:30 CDT" to "Tue, 3 Jun 2008 11:05:30 -0500",
+            "Tue, 3 Jun 2008 11:05:30 CST" to "Tue, 3 Jun 2008 11:05:30 -0600",
+            "Tue, 3 Jun 2008 11:05:30 MDT" to "Tue, 3 Jun 2008 11:05:30 -0600",
+            "Tue, 3 Jun 2008 11:05:30 MST" to "Tue, 3 Jun 2008 11:05:30 -0700",
+            "Tue, 3 Jun 2008 11:05:30 PDT" to "Tue, 3 Jun 2008 11:05:30 -0700",
+            "Tue, 3 Jun 2008 11:05:30 PST" to "Tue, 3 Jun 2008 11:05:30 -0800",
+        )
+
+        fun String.parseExpected() =
+            ZonedDateTime.parse(this, DateTimeFormatter.RFC_1123_DATE_TIME).toInstant()
+
+        (regularDates.map { it to it } + nonStandardDatesWithExpected)
+            .forEach { (dateString, expectedDateString) ->
+                assertEquals(
+                    "Incorrectly parsed $dateString",
+                    expectedDateString.parseExpected(),
+                    dateString.parseRssDate()
+                )
+            }
+    }
+
+    @Test
+    fun `parsing invalid date doesn't throw`() {
+        val invalidDates = listOf(
+            "invalid",
+            "Tue, 3 Jun 2008 11:05:30",
+            "Tue, 3 Jun 2008 11:05:30 ABC",
+            "Tue, 3 Jun 2008 11:05:30 ",
+        )
+        invalidDates.forEach { assertNull(it.parseRssDate()) }
+    }
+}
